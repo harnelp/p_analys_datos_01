1a057f5ca5939132da1d0cfe6ddba16830b18d07
Harnel <harnelp@gmail.com>
2024-02-10 00:46:12 -0500

    subiendo parte 11

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
-# Cargar el DataFrame
+# Cargamos el DataFrame
 df_limpio = pd.read_csv('p_analys_datos_05/datos_limpios.csv')
 
-# Eliminar las columnas 'DEATH_EVENT', 'age' y 'categoria_edad' para crear la matriz X
-X = df_limpio.drop(columns=['DEATH_EVENT', 'age', 'categoria_edad'])
+# Eliminar la columna 'categoria_edad' para crear la matriz X y y
+X = df_limpio.drop(columns=['DEATH_EVENT', 'categoria_edad'])
+y = df_limpio['DEATH_EVENT']
 
-# Usar la columna 'age' como vector y
-y = df_limpio['age']
+# Graficar la distribución de clases
+plt.figure(figsize=(8, 5))
+df_limpio['DEATH_EVENT'].value_counts().plot(kind='bar', color=['blue', 'orange'])
+plt.title('Distribución de Clases')
+plt.xlabel('Clase')
+plt.ylabel('Frecuencia')
+plt.xticks(ticks=[0, 1], labels=['Vivo', 'Muerto'], rotation=0)
+plt.show()
 
-# Dividir los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Partición estratificada del dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-# Ajustar una regresión lineal
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+# Ajustar un árbol de decisión
+dt_classifier = DecisionTreeClassifier(random_state=42)  # Puedes variar los parámetros aquí
+dt_classifier.fit(X_train, y_train)
 
-# Predecir las edades en el conjunto de prueba
-y_pred = regressor.predict(X_test)
+# Predecir sobre el conjunto de test
+y_pred = dt_classifier.predict(X_test)
 
-# Comparar las edades reales y predichas
-comparacion = pd.DataFrame({'Edad Real': y_test, 'Edad Predicha': y_pred})
-print(comparacion.head())  # Mostrar las primeras filas para comparar
+# Calcular el accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy del modelo: {accuracy:.2f}")
 
-# Calcular el error cuadrático medio
-mse = mean_squared_error(y_test, y_pred)
-print("")
-print(f"El error cuadrático medio (MSE) es: {mse}")
-print("")
+# Experimentar con diferentes valores de parámetros para el árbol de decisión
+# Por ejemplo, cambiar la profundidad máxima (max_depth), criterio, min_samples_split, etc.
